1e03ae94796becb8414353ee9b05ffe6d16f00f8
Peter <peterslavaperry@outlook.com>
2022-08-06 21:27:33 -0500

    Made Basic Movement

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 window.onresize = changeWindow;
+const game = new Game();
 
 function load() {
   canvas = document.querySelector('.canvas');
   ctx = canvas.getContext('2d');
   canvas.width = width;
   canvas.height = height;
-  document.onkeydown = keyPress;
+  document.onkeydown = keyDown;
+  document.onkeyup = keyUp;
+  runFrame();
 }
 
 function runFrame() {
   //DO ALL DRAWING HERE
-
+  ctx.clearRect(0, 0, width, height);
+  game.step();
+  game.draw();
   requestAnimationFrame(runFrame);
 }
 
@@ -20,9 +25,39 @@ function changeWindow() {
   //REDRAW SCREEN
 }
 
-function keyPress(key) {
-  if(key.keyCode != 32) {
-    return;
+function keyDown(key) {
+  if(key.keyCode == 65) {
+    input.ll = true;
+  }
+  if(key.keyCode == 83) {
+    input.l = true;
+  }
+  if(key.keyCode == 68) {
+    input.r = true;
+  }
+  if(key.keyCode == 70) {
+    input.rr = true;
+  }
+  if(key.keyCode == 32) {
+    space = true;
+  }
+}
+
+function keyUp(key) {
+  if(key.keyCode == 65) {
+    input.ll = false;
+  }
+  if(key.keyCode == 83) {
+    input.l = false;
+  }
+  if(key.keyCode == 68) {
+    input.r = false;
+  }
+  if(key.keyCode == 70) {
+    input.rr = false;
+  }
+  if(key.keyCode == 32) {
+    space = false;
   }
 }
 
--- setup.js
+++ setup.js
@@ -1,6 +1,9 @@
 //Vars
 let width = window.innerWidth;
 let height = window.innerHeight;
+const input = {ll: false, l: false, r: false, rr: false};
+const g = 3;
+let space = false;
 let canvas;
 let ctx;
 
@@ -80,6 +83,14 @@ function shuffle(arr) {
   return arr;
 }
 
+function meterToPix(n) {
+  return n * 20;
+}
+
+function pixToMeter(n) {
+  return n / 20;
+}
+
 //Classes
 class Vector {
   constructor(x = 0, y = 0, x0 = 0, y0 = 0) {
@@ -129,5 +140,192 @@ class Vector {
     this.x -= vector.x;
     this.y -= vector.y;
     this.getMag();
+  }
+}
+
+class Game {
+  constructor() {
+    this.player = new Player();
+    this.maxVel = meterToPix(1)
+    this.width = meterToPix(250);
+    this.height = meterToPix(50);
+    this.sideBuffer = meterToPix(10);
+    this.bottomBuffer = meterToPix(4);
+    this.houses = [];
+    this.addHouse(50);
+  }
+
+  addHouse(n = 1) {
+    for(let i = 0; i < n; i++) {
+      const house = [];
+      const width = rand(10, 20);
+      const height = rand(20, 40);
+      const x = rand(0, pixToMeter(this.width) - width);
+      house.push(meterToPix(width));
+      house.push(meterToPix(height));
+      house.push(meterToPix(x));
+      this.houses.push(house);
+    }
+  }
+
+  step() {
+    const grav = new Vector(0, -meterToPix(g));
+    this.player.applyForce(grav, 60);
+
+    if(space) {
+      for(let dir in this.player.grapples) {
+        const grapple = this.player.grapples[dir];
+        if(grapple && !grapple.shooting) {
+          const force = new Vector(grapple.endPos.x, grapple.endPos.y, this.player.pos.x, this.player.pos.y);
+          force.setMag(meterToPix(4));
+          this.player.applyForce(force, 60);
+        }
+      }
+    }
+
+    this.player.vel.add(this.player.accel);
+    this.player.vel.limit(this.maxVel);
+
+    this.player.pos.add(this.player.vel);
+    this.player.accel = new Vector();
+
+    if(this.player.pos.y - this.player.height / 2 < 0) {
+      this.player.pos.y = this.player.height / 2;
+      this.player.vel.y = 0;
+    }
+    if(this.player.pos.x + this.player.height / 2 > this.width) {
+      this.player.pos.x = this.width - this.player.height / 2;
+      this.player.vel.x = 0;
+    }
+    if(this.player.pos.x - this.player.height / 2 < 0) {
+      this.player.pos.x = this.player.height / 2;
+      this.player.vel.x = 0;
+    }
+
+    for(let dir in input) {
+      if(input[dir] != !!this.player.grapples[dir]) {
+        if(input[dir] && this.player.numGrapples < 2) {
+          this.player.numGrapples += 1;
+          this.player.grapples[dir] = new Grapple(dir, this.player.grappleAngle);
+        }
+        if(!input[dir]) {
+          this.player.numGrapples -= 1;
+          this.player.grapples[dir] = false;
+        }
+      }
+      if(this.player.grapples[dir]) {
+        const grapple = this.player.grapples[dir];
+        if(grapple.shooting) {
+          grapple.endPos.add(grapple.vel);
+          if(grapple.endPos.mag > grapple.length) {
+            grapple.shooting = false;
+            grapple.endPos.limit(grapple.length);
+            grapple.endPos.add(this.player.pos);
+          }
+        }
+      }
+    }
+  }
+
+  draw() {
+    ctx.save();
+    ctx.transform(1, 0, 0, -1, 0, canvas.height);
+
+    let point = [this.player.pos.x - (width / 2), this.player.pos.y - (height / 2)];
+    point[0] = min(max(point[0], -this.sideBuffer), this.width + this.sideBuffer - width);
+    point[1] = min(max(-this.bottomBuffer, point[1]), this.height - height);
+    ctx.translate(-point[0], -point[1]);
+
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+    ctx.moveTo(-this.sideBuffer, 0);
+    ctx.lineTo(this.width + this.sideBuffer, 0);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, this.height);
+    ctx.moveTo(this.width, 0);
+    ctx.lineTo(this.width, this.height);
+    ctx.stroke();
+
+    for(let house of this.houses) {
+      ctx.clearRect(house[2], 1, house[0], house[1]);
+      ctx.beginPath();
+      ctx.moveTo(house[2], 0);
+      ctx.lineTo(house[2], house[1]);
+      ctx.lineTo(house[2] + house[0], house[1]);
+      ctx.lineTo(house[2] + house[0], 0);
+      ctx.stroke();
+    }
+
+    ctx.beginPath();
+    ctx.arc(this.player.pos.x, this.player.pos.y, this.player.height / 2, 0, Math.PI * 2);
+    ctx.stroke();
+
+    for(const dir in this.player.grapples) {
+      const grapple = this.player.grapples[dir];
+      if(!grapple) {
+        continue;
+      }
+      if(grapple.shooting) {
+        ctx.beginPath();
+        ctx.moveTo(this.player.pos.x, this.player.pos.y);
+        ctx.lineTo(grapple.endPos.x + this.player.pos.x, grapple.endPos.y + this.player.pos.y);
+        ctx.stroke();
+      } else {
+        ctx.beginPath();
+        ctx.moveTo(this.player.pos.x, this.player.pos.y);
+        ctx.lineTo(grapple.endPos.x, grapple.endPos.y);
+        ctx.stroke();
+      }
+    }
+
+    ctx.restore();
+  }
+}
+
+class Player {
+  constructor() {
+    this.pos = new Vector(500, 500);
+    this.vel = new Vector();
+    this.height = meterToPix(2);
+    this.accel = new Vector(0, 0);
+    this.grapples = {ll: false, l: false, r: false, rr: false};
+    this.numGrapples = 0;
+    this.grappleAngle = 140;
+    this.onGround = false;
+  }
+
+  applyForce(force, dt) {
+    force.x /= dt;
+    force.y /= dt;
+    force.getMag();
+    this.accel.add(force);
+  }
+
+}
+
+class Grapple {
+  constructor(dir, angleRange) {
+    this.endPos = new Vector();
+    this.length = meterToPix(50);
+    let angle = 0;
+    switch(dir) {
+      case 'll':
+        angle = 90 + angleRange / 2;
+        break;
+      case 'l':
+        angle = 90 + angleRange / 4;
+        break;
+      case 'r':
+        angle = 90 - angleRange / 4;
+        break;
+      case 'rr':
+        angle = 90 - angleRange / 2;
+        break;
+    }
+    this.vel = new Vector();
+    this.vel.x = Math.cos(degToRad(angle)) * 100;
+    this.vel.y = Math.sin(degToRad(angle)) * 100;
+    this.vel.getMag();
+    this.shooting = true;
   }
 }
